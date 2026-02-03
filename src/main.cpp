4b1d90380ef9a5463c683272703861e6c4e3c73d
JaretEduardo <jaret.gonzalez.ct59@gmail.com>
2026-02-03 13:42:46 -0600

    feat: Implementation of MNIST logic and Batch support in Trainer

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 ﻿#include <torch/torch.h>
 #include <iostream>
-#include "Models/XORModel.h"
+#include "Models/VisionModel.h"
 #include "Core/Trainer.h"
 
 int main() {
-    std::cout << "=== KAI ENGINE v2.0 ===" << std::endl;
+    std::cout << "=== KAI ENGINE v2.5: Vision Module ===" << std::endl;
 
     torch::Device device(torch::kCPU);
     if (torch::cuda::is_available()) {
@@ -12,15 +12,34 @@ int main() {
         device = torch::Device(torch::kCUDA);
     }
 
-    auto cerebro = std::make_shared<XORModel>();
+    std::cout << "[DATA] Cargando imagenes MNIST..." << std::endl;
+
+    auto dataset = torch::data::datasets::MNIST("assets/datasets/mnist")
+        .map(torch::data::transforms::Stack<>());
+
+    auto data_loader = torch::data::make_data_loader(
+        std::move(dataset),
+        torch::data::DataLoaderOptions().batch_size(64).workers(2)
+    );
+
+    auto cerebro = std::make_shared<VisionModel>();
     cerebro->to(device);
 
-    auto inputs = torch::tensor({ {0.0f, 0.0f}, {0.0f, 1.0f}, {1.0f, 0.0f}, {1.0f, 1.0f} }).to(device);
-    auto targets = torch::tensor({ {0.0f}, {1.0f}, {1.0f}, {0.0f} }).to(device);
+    std::cout << "[MODELO] VisionModel (LeNet) inicializado." << std::endl;
 
-    torch::optim::Adam optimizador(cerebro->parameters(), torch::optim::AdamOptions(0.01));
+    torch::optim::SGD optimizador(cerebro->parameters(), torch::optim::SGDOptions(0.01).momentum(0.5));
 
-    Trainer::Train(cerebro, inputs, targets, optimizador, 2000, "assets/checkpoints/xor_brain.pt");
+    std::cout << "[ENTRENAMIENTO] Iniciando lectura de 60,000 imagenes..." << std::endl;
+
+    Trainer::Train(
+        cerebro,
+        data_loader,
+        optimizador,
+        device,
+        10,
+        60000,
+        "assets/checkpoints/vision_brain.pt"
+    );
 
     std::cout << "\nPresiona Enter para salir.";
     std::cin.get();
